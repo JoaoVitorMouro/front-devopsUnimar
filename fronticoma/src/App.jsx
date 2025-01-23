89ab18b4003a43ff3b45be2e39359f42eff7578b
Joao Mouro <joaovitorbarboza1@hotmail.com>
2025-01-23 15:31:47 -0300

    v2

--- fronticoma/src/App.jsx
+++ fronticoma/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>JOAO MOURO APP</h1>
+      <h1>JOAO MOURO APP v2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
